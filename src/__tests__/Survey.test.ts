4f9df186c21398bb3bdfd60e565fd38c986e074d
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2021-03-01 18:58:46 -0300

    add Survey test

---
+++ src/__tests__/Survey.test.ts
@@ -0,0 +1,37 @@
+import request from 'supertest';
+import { getConnection } from 'typeorm';
+import { app } from '../app';
+import createConnection from '../database';
+
+describe('Survey', () => {
+  beforeAll(async () => {
+    const connection = await createConnection();
+    await connection.runMigrations();
+  });
+
+  afterAll(async () => {
+    const connection = getConnection();
+    await connection.dropDatabase();
+    await connection.close();
+  });
+
+  it('Should be able to create a new survey', async () => {
+    const response = await request(app).post('/surveys').send({
+      title: 'Title example',
+      description: 'Description Example',
+    });
+    expect(response.status).toBe(201);
+    expect(response.body).toHaveProperty('id');
+  });
+
+  it('Should be able to get all surveys', async () => {
+    await request(app).post('/surveys').send({
+      title: 'Title example',
+      description: 'Description Example',
+    });
+
+    const response = await request(app).get('/surveys');
+
+    expect(response.body.length).toBe(2);
+  });
+});
