541cdae01bcda3f3c28298b64153b65f937e3bfc
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2021-03-01 18:46:56 -0300

    add AppError handle

---
+++ src/errors/AppError.ts
@@ -0,0 +1,12 @@
+class AppError {
+  message: string;
+
+  statusCode: number;
+
+  constructor(message: string, statusCode = 400) {
+    this.message = message;
+    this.statusCode = statusCode;
+  }
+}
+
+export default AppError;
